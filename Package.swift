9e26efa2c057e3518a612e327e5148b3c1edd624
cosmocolor <153699447+cosmocolor@users.noreply.github.com>
2024-02-23 00:48:35 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,12 +16,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
         .package(url: "https://github.com/valpackett/SwiftCBOR", from: "0.4.6"),
-        .package(url: "https://github.com/auth0/JWTDecode.swift", from: "3.1.0")
+        .package(url: "https://github.com/auth0/JWTDecode.swift.git", from: "3.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ISOMDLLib", url: "https://storage.googleapis.com/cosmo-ios-repo/ISOMDLLib.xcframework.zip", checksum: "9e08725b0b113763dea5d77debee01eac57606a8b55bd5ecae37604cd1a60197"),
+        .binaryTarget(name: "ISOMDLLib", 
+                      dependencies: ["SwiftyJSON", "SwiftCBOR", .product(name: "JWTDecode", package: "jwtdecode.swift")],
+                      url: "https://storage.googleapis.com/cosmo-ios-repo/ISOMDLLib.xcframework.zip", checksum: "9e08725b0b113763dea5d77debee01eac57606a8b55bd5ecae37604cd1a60197"),
        
     ]
 )
