52bc47556339425412174058a14f3b264c02fe2b
cosmocolor <153699447+cosmocolor@users.noreply.github.com>
2024-02-23 00:20:42 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Iso18013mdl-SDK",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v17)
         ],
     
     products: [
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "Iso18013mdl-SDK", url: "https://storage.googleapis.com/cosmo-ios-repo/Iso18013Sdk.xcframework.zip", checksum: "5f1806851aba2ca19cf1cf15a2342c8bd719f8b46d87c0559cfec0ff0006a60b"),
+        .binaryTarget(name: "Iso18013mdl-SDK", url: "https://storage.googleapis.com/cosmo-ios-repo/Iso18013Sdk.xcframework.zip", checksum: "9e08725b0b113763dea5d77debee01eac57606a8b55bd5ecae37604cd1a60197"),
        
     ]
 )
