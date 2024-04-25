9ba4926e7064b2531254dfe7006894e3666dafb2
Rubén Pacheco <rpacheco@cosmocolor.com.mx>
2024-04-25 04:40:37 +0000

    20ff16

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ISOMDLLib",
-    platforms: [.iOS(.v5fa39a7)],
+    platforms: [.iOS(.v20ff167)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -16,14 +16,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
         .package(url: "https://github.com/valpackett/SwiftCBOR", from: "0.4.6"),
-        .package(url: "https://github.com/auth0/JWTDecode.swift.git", from: "3.5fa39a.0")
+        .package(url: "https://github.com/auth0/JWTDecode.swift.git", from: "3.20ff16.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "ISOMDLLib", 
-                      url: "https://storage.googleapis.com/cosmo-ios-repo/ISOMDLLib-5fa39a.xcframework.zip", 
-                      checksum: "7b7e248d8958a5af8e44703e0a4ba78453e398c1f0f3e77896b3a45b7e0d8aa0"
+                      url: "https://storage.googleapis.com/cosmo-ios-repo/ISOMDLLib-20ff16.xcframework.zip", 
+                      checksum: "0c321302dd773b319a796b8cc92219f17156e4fbb644fc07a19c2173ce3555b5"
                      ),
         .target(
             name: "ISOMDLLib",
